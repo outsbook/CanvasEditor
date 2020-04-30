cc42f617417071ebdc905c368d36dbb8f82b3c4d
Shahin shamS <shamscse@gmail.com>
2020-04-30 12:47:53 +0600

    remove lib

--- canvaseditor/src/main/java/com/outsbook/libs/canvaseditor/enums/DrawType.kt
+++
@@ -1,6 +0,0 @@
-package com.outsbook.libs.canvaseditor.enums
-
-internal enum class DrawType {
-    PATH,
-    STICKER
-}
--- canvaseditor/src/main/java/com/outsbook/libs/canvaseditor/models/PathAndPaint.kt
+++
@@ -1,9 +0,0 @@
-package com.outsbook.libs.canvaseditor.models
-
-import android.graphics.Paint
-import android.graphics.Path
-
-internal data class PathAndPaint(
-    val path: Path,
-    val paint: Paint
-)
